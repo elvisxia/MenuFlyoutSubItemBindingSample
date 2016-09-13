74ea6d360c12c5bf1af48d150476b598698f4bdd
v-xucxia <v-xucxia@PC-WINFFEE>
2016-09-13 10:21:13 +0800

    Add project files.

---
+++ MenuFlyoutSubItemBindingSample/MainPage.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MenuFlyoutSubItemBindingSample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page,INotifyPropertyChanged
+    {
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaiseProperty(string name) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        public List<MenuFlyoutItemBase> OptionItems { get; set; }
+
+        public List<MenuFlyoutItemBase> InitFlyoutItems()
+        {
+            var list = new List<MenuFlyoutItemBase>
+            {
+                new MenuFlyoutItem {Text="Start item 1" },
+                new MenuFlyoutItem {Text="Start item 2" },
+                new MenuFlyoutItem {Text="Start item 3" },
+                new MenuFlyoutSubItem { Text="Start Item 4"  }
+            };
+            ((MenuFlyoutSubItem)list[3]).Items.Add(new MenuFlyoutItem { Text = "Old Sub Item 1" });
+            ((MenuFlyoutSubItem)list[3]).Items.Add(new MenuFlyoutItem { Text = "Old Sub Item 2" });
+            ((MenuFlyoutSubItem)list[3]).Items.Add(new MenuFlyoutItem { Text = "Old Sub Item 3" });
+            ((MenuFlyoutSubItem)list[3]).Items.Add(new MenuFlyoutItem { Text = "Old Sub Item 4" });
+            return list;
+        }
+
+        public List<MenuFlyoutItemBase> UpdateFlyoutItems()
+        {
+            var list = new List<MenuFlyoutItemBase>
+            {
+                new MenuFlyoutItem {Text="Start item 1" },
+                new MenuFlyoutItem {Text="Start item 2" },
+                new MenuFlyoutItem {Text="Start item 3" },
+                new MenuFlyoutSubItem { Text="Start Item 4"  }
+            };
+            ((MenuFlyoutSubItem)list[3]).Items.Add(new MenuFlyoutItem { Text = "New Sub Item 1" });
+            ((MenuFlyoutSubItem)list[3]).Items.Add(new MenuFlyoutItem { Text = "New Sub Item 2" });
+            ((MenuFlyoutSubItem)list[3]).Items.Add(new MenuFlyoutItem { Text = "New Sub Item 3" });
+            ((MenuFlyoutSubItem)list[3]).Items.Add(new MenuFlyoutItem { Text = "New Sub Item 4" });
+            return list;
+        }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            DataContext = this;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            OptionItems = InitFlyoutItems();
+            RaiseProperty(nameof(OptionItems));
+            base.OnNavigatedTo(e);
+        }
+
+
+        //int index = 0;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OptionItems=UpdateFlyoutItems();
+            RaiseProperty(nameof(OptionItems));
+            
+            //mysub.Items.Add(new MenuFlyoutItem { Text = "modified" }); 
+            //int count = mysub.Items.Count;
+            //if (index >= 3)
+            //{
+            //    var subitem = new MenuFlyoutSubItem();
+            //    subitem.Items.Add(new MenuFlyoutItem { Text = "New Text" });
+            //    ((MenuFlyout)myBtn.Flyout).Items[2] = subitem;
+            //    index++;
+            //}
+            //index++;
+        }
+    }
+}
---
+++ MenuFlyoutSubItemBindingSample/MenuExtension.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace MenuFlyoutSubItemBindingSample
+{
+    //public static class MenuExtension
+    //{
+    //    public static List<MenuFlyoutItem> GetMyItems(DependencyObject obj)
+    //    { return (List<MenuFlyoutItem>)obj.GetValue(MyItemsProperty); }
+
+    //    public static void SetMyItems(DependencyObject obj, List<MenuFlyoutItem> value)
+    //    { obj.SetValue(MyItemsProperty, value); }
+
+    //    public static readonly DependencyProperty MyItemsProperty =
+    //        DependencyProperty.Register("MyItems", typeof(List<MenuFlyoutItem>), typeof(MenuExtension),
+    //        new PropertyMetadata(new List<MenuFlyoutItem>(), (sender, e) =>
+    //        {
+    //            Debug.WriteLine("Filling collection");
+    //            var menu = sender as MenuFlyoutSubItem;
+    //            menu.Items.Clear();
+    //            foreach (var item in e.NewValue as List<MenuFlyoutItem>) menu.Items.Add(item);
+    //        }));
+    //}
+
+    public static class FlyoutMenuExtension
+    {
+        public static List<MenuFlyoutItemBase> GetMyItems(DependencyObject obj)
+        {
+            return (List<MenuFlyoutItemBase>)obj.GetValue(MyItemsProperty);
+        }
+        public static void SetMyItems(DependencyObject obj, List<MenuFlyoutItemBase> value)
+        {
+            obj.SetValue(MyItemsProperty, value);
+        }
+        public static readonly DependencyProperty MyItemsProperty =
+            DependencyProperty.Register("MyItems",
+                typeof(List<MenuFlyoutItemBase>),
+                typeof(FlyoutMenuExtension),
+                new PropertyMetadata(new List<MenuFlyoutItemBase>(), (sender, e) => {
+                    var menu = sender as MenuFlyout;
+                    menu.Items.Clear();
+                    foreach (var item in e.NewValue as List<MenuFlyoutItemBase>)
+                    {
+                        menu.Items.Add(item);
+                    }
+                }));
+    }
+}
